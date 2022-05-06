b7c9f169d0ce47ac8c9afa4d5e7aebc9146baa8a
Mostafa Musab Naveed <mustafanaveed404@gmail.com>
2022-05-06 14:58:03 +0800

    app unable to run

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'pages/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
-import 'pages/splash_screen.dart';
 
-void main() {
+
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(LoginUiApp());
 }
 
