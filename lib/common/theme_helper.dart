be3aa8ee6114aa1889ab3ad9c441d512debde6fe
Mostafa Musab Naveed <mustafanaveed404@gmail.com>
2022-05-12 12:19:29 +0800

    sprint 1 2.0v

--- lib/common/theme_helper.dart
+++ lib/common/theme_helper.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
+// this widget would be common for all 
 class ThemeHelper {
   InputDecoration textInputDecoration(
       [String lableText = "", String hintText = ""]) {
