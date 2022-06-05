683b9be3f5b30a4a0d9c9fac99bd7cf02c2d88d6
Mostafa Musab Naveed <mustafanaveed404@gmail.com>
2022-06-05 22:22:38 +0800

    Product description page merged

---
+++ lib/screens/details/components/Single_item_body.dart
@@ -0,0 +1,437 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_login_ui/constants.dart';
+import 'package:flutter_login_ui/models/product_model.dart';
+import '../checkout/check_out_page.dart';
+import '/route/routing_page.dart';
+
+import 'package:flutter_login_ui/route/routing_page.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../cartPage/cart_page.dart';
+
+class ProductTitleWithImage extends StatelessWidget {
+  const ProductTitleWithImage({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  final Product? product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const Text(
+            "Great Food",
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            product!.name,
+            style: Theme.of(context)
+                .textTheme
+                .headline4!
+                .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: kDefaultPaddin),
+          Row(
+            children: <Widget>[
+              RichText(
+                text: TextSpan(
+                  children: [
+                    const TextSpan(text: "Price\n"),
+                    TextSpan(
+                      text: "RM${product!.price}",
+                      style: Theme.of(context).textTheme.headline4!.copyWith(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(width: kDefaultPaddin),
+              Expanded(
+                child: SizedBox(
+                  height: 200,
+                  width: 200,
+                  //border radius
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network(
+                      product!.imageUrl,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SingleItemBody extends StatelessWidget {
+  final Product? product;
+
+  const SingleItemBody({Key? key, this.product}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // It provide us total height and width
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: size.height,
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: size.height * .3),
+                  padding: EdgeInsets.only(
+                    top: size.height * 0.12,
+                    left: kDefaultPaddin,
+                    right: kDefaultPaddin,
+                  ),
+                  // height: 500,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(24),
+                      topRight: Radius.circular(24),
+                    ),
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      ColorAndSize(product: product),
+                      const SizedBox(height: kDefaultPaddin / 2),
+                      Description(product: product),
+                      const SizedBox(height: kDefaultPaddin / 2),
+                      const CounterWithFavBtn(),
+                      const SizedBox(height: kDefaultPaddin / 2),
+                      AddToCart(product: product)
+                    ],
+                  ),
+                ),
+                ProductTitleWithImage(product: product)
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class AddToCart extends StatelessWidget {
+  const AddToCart({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  final Product? product;
+
+  get productImage => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: kDefaultPaddin),
+      child: Row(
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(right: kDefaultPaddin),
+            height: 50,
+            width: 58,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(18),
+              border: Border.all(
+                color: (product!.isRecommended == true)
+                    ? const Color.fromARGB(255, 255, 124, 17)
+                    : const Color.fromARGB(255, 41, 41, 41),
+              ),
+            ),
+            child: IconButton(
+              icon: SvgPicture.asset(
+                "assets/icons/add_to_cart.svg",
+                color: (product!.isRecommended == true)
+                    ? const Color.fromARGB(255, 255, 124, 17)
+                    : const Color.fromARGB(255, 41, 41, 41),
+              ),
+              onPressed: () {
+                FirebaseFirestore.instance
+                    .collection("cart")
+                    .doc(FirebaseAuth.instance.currentUser!.uid)
+                    .collection("userCart")
+                    .doc(product!.id.toString())
+                    .set(
+                  {
+                    "id": product!.id,
+                    "name": product!.name,
+                    "price": product!.price,
+                    "imageUrl": product!.imageUrl,
+                    "description": product!.description,
+                    "category": product!.category,
+                    "quantity": 1,
+                  },
+                );
+
+                RoutingPage.goTonext(
+                  context: context,
+                  navigateTo: const CartPage(),
+                );
+              },
+            ),
+          ),
+          Expanded(
+            child: SizedBox(
+              height: 50,
+              child: FlatButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18)),
+                color: (product!.isRecommended == true)
+                    ? const Color.fromARGB(255, 255, 124, 17)
+                    : const Color.fromARGB(255, 41, 41, 41),
+                onPressed: () {
+                  FirebaseFirestore.instance
+                      .collection("cart")
+                      .doc(FirebaseAuth.instance.currentUser!.uid)
+                      .collection("userCart")
+                      .doc(product!.id.toString())
+                      .set(
+                    {
+                      "id": product!.id,
+                      "name": product!.name,
+                      "price": product!.price,
+                      "imageUrl": product!.imageUrl,
+                      "description": product!.description,
+                      "category": product!.category,
+                      "quantity": 1,
+                    },
+                  );
+
+                  RoutingPage.goTonext(
+                    context: context,
+                    navigateTo: const CheckOutPage(),
+                  );
+                },
+                child: Text(
+                  "Buy  Now".toUpperCase(),
+                  style: const TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CartCounter extends StatefulWidget {
+  const CartCounter({Key? key}) : super(key: key);
+
+  @override
+  _CartCounterState createState() => _CartCounterState();
+}
+
+class _CartCounterState extends State<CartCounter> {
+  int numOfItems = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        buildOutlineButton(
+          icon: Icons.remove,
+          press: () {
+            if (numOfItems > 1) {
+              setState(() {
+                numOfItems--;
+              });
+            }
+          },
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin / 2),
+          child: Text(
+            // if our item is less  then 10 then  it shows 01 02 like that
+            numOfItems.toString().padLeft(2, "0"),
+            style: Theme.of(context).textTheme.headline6,
+          ),
+        ),
+        buildOutlineButton(
+            icon: Icons.add,
+            press: () {
+              setState(() {
+                numOfItems++;
+              });
+            }),
+      ],
+    );
+  }
+
+  SizedBox buildOutlineButton({IconData? icon, Function? press}) {
+    return SizedBox(
+      width: 40,
+      height: 32,
+      child: OutlinedButton(
+        style: ButtonStyle(
+          padding: MaterialStateProperty.all(
+            EdgeInsets.zero,
+          ),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(13),
+              ),
+            ),
+          ),
+        ),
+        onPressed: press as void Function()?,
+        child: Icon(icon),
+      ),
+    );
+  }
+}
+
+class ColorAndSize extends StatelessWidget {
+  const ColorAndSize({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  final Product? product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text("Color"),
+              Row(
+                children: const <Widget>[
+                  ColorDot(
+                    color: Color(0xFF356C95),
+                    isSelected: true,
+                  ),
+                  ColorDot(color: Color(0xFFF8C078)),
+                  ColorDot(color: Color(0xFFA29B9B)),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: RichText(
+            text: TextSpan(
+              style: const TextStyle(color: kTextColor),
+              children: [
+                const TextSpan(text: "Size\n"),
+                TextSpan(
+                  text: "${product!.quantity} cm",
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline5!
+                      .copyWith(fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ColorDot extends StatelessWidget {
+  final Color? color;
+  final bool isSelected;
+  const ColorDot({
+    Key? key,
+    this.color,
+    // by default isSelected is false
+    this.isSelected = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: kDefaultPaddin / 4,
+        right: kDefaultPaddin / 2,
+      ),
+      padding: const EdgeInsets.all(2.5),
+      height: 24,
+      width: 24,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: isSelected ? color! : Colors.transparent,
+        ),
+      ),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: color,
+          shape: BoxShape.circle,
+        ),
+      ),
+    );
+  }
+}
+
+class CounterWithFavBtn extends StatelessWidget {
+  const CounterWithFavBtn({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        CartCounter(),
+        Container(
+          padding: const EdgeInsets.all(8),
+          height: 32,
+          width: 32,
+          decoration: const BoxDecoration(
+            color: Color(0xFFFF6464),
+            shape: BoxShape.circle,
+          ),
+          child: SvgPicture.asset("assets/icons/heart.svg"),
+        )
+      ],
+    );
+  }
+}
+
+class Description extends StatelessWidget {
+  const Description({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  final Product? product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: kDefaultPaddin),
+      child: Text(
+        product!.description,
+        style: const TextStyle(height: 1.5),
+      ),
+    );
+  }
+}
