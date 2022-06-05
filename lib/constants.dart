55552c9e986b2c52121477c966e7712deea1fd66
Abu Sayed <76618493+romanyad@users.noreply.github.com>
2022-06-05 22:10:27 +0800

    app_properties

--- lib/constants.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kTextColor = Color.fromARGB(255, 50, 50, 50);
-const kTextLightColor = Color(0xFFACACAC);
-
-const kDefaultPaddin = 20.0;
--- lib/models/model.dart
+++
@@ -1,2 +0,0 @@
-export 'product_model.dart';
-export 'category_model.dart';
